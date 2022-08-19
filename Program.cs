66a89ecaaee98c3e11a01829486611c6454f14c1
GabrielSilvaMEL <gabrielsilvaalves3000@outlook.com>
2022-08-19 12:10:41 -0300

    menu 2

--- Program.cs
+++ Program.cs
@@ -3,15 +3,38 @@ Console.WriteLine("MENU PRINCIPAL");
 Console.WriteLine("1 - Funções sem vetor");
 Console.WriteLine("2 - Funções com vetor");
 Console.WriteLine("##############");
-op1 = 0;
+int op1 = 0;
 op1 = Console.Readline("Digite a opção desejada: ");
 
-while {op1 != 3}{
-    if op1 = 1{
-        n1 = 0;
-        n2 = 0;
-        n1 = Console.ReadLine{"Digite um número inicial: "};
-        n2 = Console.ReadLine{"Digite o número final: "};
-        
+while (op1 != 3){
+    Console.Clear();
+    if (op1 = 1){
+        int n1 = 0;
+        int n2 = 0;
+        int op2 = 0;
+        n1 = int.Console.ReadLine{"Digite um número inicial: "};
+        n2 = int.Console.ReadLine{"Digite o número final: "};
+        Console.WriteLine("O que deseja fazer com esses números? ");
+        Console.WriteLine("1 - Crescente");
+        Console.WriteLine("2 - Decrescente");
+        Console.WriteLine("3 - Impares");
+        Console.WriteLine("4 - Somatório");
+        Console.Write("Opção desejada: ");
+        op2 = Console.ReadLine{};
+
+        //opção 1
+        if (op2 = 1){
+            if(n1 < n2){
+                
+            }
+            else{
+
+            }
+        }
+
+        //opção 2
+        if (op2 = 2){
+
+        }
     }
 }
