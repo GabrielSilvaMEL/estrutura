34bd5e74c4e61dd3382c9685c932abd93e9006fc
GabrielSilvaMEL <gabrielsilvaalves3000@outlook.com>
2022-08-19 11:57:07 -0300

    menu

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("MENU PRINCIPAL");
+Console.WriteLine("1 - Funções sem vetor");
+Console.WriteLine("2 - Funções com vetor");
+Console.WriteLine("##############");
+op1 = 0;
+op1 = Console.Readline("Digite a opção desejada: ");
+
+while {op1 != 3}{
+    if op1 = 1{
+        n1 = 0;
+        n2 = 0;
+        n1 = Console.ReadLine{"Digite um número inicial: "};
+        n2 = Console.ReadLine{"Digite o número final: "};
+        
+    }
+}
