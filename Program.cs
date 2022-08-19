3d0efab07856f4e93f29f55cc74974185ba502c8
GabrielSilvaMEL <gabrielsilvaalves3000@outlook.com>
2022-08-19 12:20:48 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,39 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("MENU PRINCIPAL");
-Console.WriteLine("1 - Funções sem vetor");
-Console.WriteLine("2 - Funções com vetor");
-Console.WriteLine("##############");
-int op1 = 0;
-op1 = Console.Readline("Digite a opção desejada: ");
-
-while (op1 != 3){
+﻿string op1 = "0";
+while (op1 != "3"){
     Console.Clear();
-    if (op1 = 1){
+    Console.WriteLine("MENU PRINCIPAL");
+    Console.WriteLine("1 - Funções sem vetor");
+    Console.WriteLine("2 - Funções com vetor");
+    Console.WriteLine("Digite a opção desejada:");
+    op1 = Console.ReadLine();
+    if (op1 == "1"){
         int n1 = 0;
         int n2 = 0;
         int op2 = 0;
-        n1 = int.Console.ReadLine{"Digite um número inicial: "};
-        n2 = int.Console.ReadLine{"Digite o número final: "};
+
+        Console.WriteLine("Digite um número inicial: ");
+        n1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o número final: ");
+        n2 = int.Parse(Console.ReadLine());
+
         Console.WriteLine("O que deseja fazer com esses números? ");
         Console.WriteLine("1 - Crescente");
         Console.WriteLine("2 - Decrescente");
         Console.WriteLine("3 - Impares");
         Console.WriteLine("4 - Somatório");
         Console.Write("Opção desejada: ");
-        op2 = Console.ReadLine{};
+        op2 = int.Parse(Console.ReadLine());
 
         //opção 1
-        if (op2 = 1){
-            if(n1 < n2){
-                
-            }
-            else{
-
-            }
-        }
+        
 
         //opção 2
-        if (op2 = 2){
+        if (op2 == 2){
 
         }
     }
