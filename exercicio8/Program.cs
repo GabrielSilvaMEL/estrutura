42140024ee121bfb56c01d19141276c4d40411ed
GabrielSilvaMEL <gabrielsilvaalves3000@outlook.com>
2022-10-25 19:30:58 -0300

    Create Program.cs

---
+++ exercicio8/Program.cs
@@ -0,0 +1,46 @@
+﻿const int MAX = 20;
+
+void Insere(int[] p, ref int t, int v)
+{
+    p[t] = v;
+    t = t + 1;
+}
+
+int Remove(int[] p, ref int t)
+{
+    t = t - 1;
+    return (p[t]);
+}
+
+bool EstaVazia(int t)
+{
+    if (t == 0)
+        return true;
+    else
+        return false;
+}
+
+bool EstaCheia(int t)
+{
+    if (t == MAX)
+        return true;
+    else
+        return false;
+}
+
+int n, r;
+int[] pilha = new int[MAX];
+int topo = 0;
+
+
+Console.Write("Digite um número");
+n = int.Parse(Console.ReadLine());
+while (n!=0){
+    Insere(pilha,ref topo, n % 2);
+    n = n/2;
+}
+
+while(!EstaVazia(topo)){
+    r = Remove(pilha,ref topo);
+    Console.Write(r);
+}
