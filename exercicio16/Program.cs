7ea2544964ac82dc2b3eac1fa73db353694b1a51
GabrielSilvaMEL <gabrielsilvaalves3000@outlook.com>
2022-10-25 19:33:42 -0300

    u´p

---
+++ exercicio16/Program.cs
@@ -0,0 +1,34 @@
+﻿tp_no Remove(ref tp_no l)
+{
+    tp_no no = null;
+    if (l != null)
+    {
+        no = l;
+        l = l.prox;
+        no.prox = null;
+    }
+    return no;
+}
+void Insere(ref tp_no l, int v)
+{
+    tp_no no = new tp_no();
+    no.valor = v;
+    if (l != null)
+        no.prox = l;
+    l = no;
+}
+
+tp_no lista = null;
+tp_no elem;
+Insere(ref lista,20);
+Insere(ref lista,30);
+Insere(ref lista,40);
+elem = Remove(ref lista);
+Console.WriteLine(elem.valor);
+elem = Remove(ref lista);
+Console.WriteLine(elem.valor);
+class tp_no
+{
+    public int valor;
+    public tp_no prox;
+}
